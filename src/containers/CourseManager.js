6d2c383d3a35f85d5e54a976728baad039b0f51c
Thomas Shockman <tom.shockman@gmail.com>
2018-07-16 21:26:19 -0400

    Add bootstrap to project

--- src/containers/CourseManager.js
+++ src/containers/CourseManager.js
@@ -3,6 +3,10 @@ import React from 'react';
 export default class CourseManager extends React.Component {
 
   render() {
-    return <h1>Shockman's Course Manager</h1>;
+    return (
+      <div className="container-fluid">
+        <h1>Shockman's Course Manager</h1>
+      </div>
+    );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CourseManager from './containers/CourseManager';
+import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(<CourseManager/>, document.getElementById('root'));
