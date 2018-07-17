ddc4b2c9525b2752253007be5e958f2e444392dc
Thomas Shockman <tom.shockman@gmail.com>
2018-07-16 21:51:11 -0400

    Add font-awesome

--- src/components/CourseListItem.js
+++ src/components/CourseListItem.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.css';
 
 export default class CourseListItem extends React.Component {
   render() {
     return (
-      <li className="list-group-item">{this.props.title}</li>
+      <li className="list-group-item">
+        {this.props.title}
+        <span className="float-right">
+          <i className="fa fa-trash"></i>
+          <i className="fa fa-pencil"></i>
+        </span>
+      </li>
     )
   }
 }
