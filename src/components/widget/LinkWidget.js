a7ec65f9aa28ecfa72a109e270a846c3643eb3d6
Thomas Shockman <tom.shockman@gmail.com>
2018-08-01 21:31:46 -0400

    Add link widget

--- src/components/widget/LinkWidget.js
+++ src/components/widget/LinkWidget.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {Row, Col} from 'reactstrap';
+import {Row, Col, Form, FormGroup, Label, Input} from 'reactstrap';
 
 export default class LinkWidget extends React.PureComponent {
   static propTypes = {
@@ -10,24 +10,54 @@ export default class LinkWidget extends React.PureComponent {
   };
 
   renderPreview = () => {
-
+    const {widget} = this.props;
+    return <a href={widget.href} target="_blank">{widget.text || widget.href}</a>
   };
 
+  handleUpdate = event => {
+    const {updateWidget, widget} = this.props;
+    const newWidget = {...widget};
+    switch (event.target.id) {
+      case 'linkText':
+        newWidget.text = event.target.value;
+        break;
+      case 'linkHref':
+        newWidget.href = event.target.value;
+        break;
+      case 'widgetName':
+        newWidget.name = event.target.value;
+        break;
+    }
+    updateWidget(newWidget);
+  };
   render() {
     const {widget, onlyPreview} = this.props;
 
     return (
       <div>
         {!onlyPreview &&
-        <Row>
+        <Row className="border">
           <Col>
-            LINK EDIT AREA
+            <Form>
+              <FormGroup>
+                <Label for="linkText">Link Text</Label>
+                <Input onChange={this.handleUpdate} type="text" id="linkText" placeholder="Link text..." value={widget.text}/>
+              </FormGroup>
+              <FormGroup>
+                <Label for="linkHref">Link URL</Label>
+                <Input onChange={this.handleUpdate} type="text" id="linkHref" placeholder="Link URL..." value={widget.href}/>
+              </FormGroup>
+              <FormGroup>
+                <Label for="widgetName">Widget Name</Label>
+                <Input onChange={this.handleUpdate} type="text" id="widgetName" placeholder="Widget name..." value={widget.name}/>
+              </FormGroup>
+            </Form>
           </Col>
         </Row>
         }
-        <Row>
+        <Row className="border">
           <Col>
-            PREVIEW AREA
+            {this.renderPreview()}
           </Col>
         </Row>
       </div>
